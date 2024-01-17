a4af923fd0a87b30348044f2debd0ff37d45de4f
Lea-MarieKindermann <92923384+LeaMarieKindermann@users.noreply.github.com>
2024-01-17 14:02:53 +0100

    Attack Hitbox

---
+++ Spieleprogrammierung_WS23-24/Assets/Skripte/HitboxActivator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxActivator : MonoBehaviour
+{
+    public WeaponHitbox weaponHitbox;
+
+    private void Start()
+    {
+        weaponHitbox = GetComponent<WeaponHitbox>();
+        if (weaponHitbox == null)
+        {
+            Debug.LogError("WeaponHitbox not found!");
+        }
+    }
+
+    // Wird im AnimationEvent aufgerufen
+    public void ActivateHitboxWithDelay()
+    {
+        Debug.Log("Hitbox activated!");
+        weaponHitbox.ActivateHitbox();
+        Invoke("DeactivateHitbox", 0.1f); // Deaktiviere die Hitbox nach 0.1 Sekunden
+    }
+
+    private void DeactivateHitbox()
+    {
+        Debug.Log("Hitbox deactivated!");
+        weaponHitbox.DeactivateHitbox();
+    }
+}
